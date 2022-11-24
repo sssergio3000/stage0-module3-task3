43f5b6c5850e8270450e99a25eea76597ccb2130
Sergey Khaykov <ssserg3000@gmail.com>
2022-11-24 18:55:46 +0200

    update1

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -3,7 +3,9 @@ package lang.print.gaps.task3;
 public class DeclaringVars {
     public static void main(String[] args) {
         //write code here
-        int a, b, c = 10;
+        int a, b, c;
+        a = b = c = 10;
         System.out.println(c);
+//        System.out.println(b);
     }
 }
